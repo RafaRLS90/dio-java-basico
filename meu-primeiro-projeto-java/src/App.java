2849391d5c1b2d3911cde610686cc54078718aec
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2022-11-21 23:47:48 -0300

    aprendendo a linguagem

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -2,4 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Bem vindo ao meu primeiro projeto JAVA");
     }
+
+    final String BR = "Brasil";
+    int ESTADOS_BRASILEIROS  = 27;
+
 }
