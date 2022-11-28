6f923c44b6a6ddeb1eedebe0948bd5d210ca397f
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2022-11-27 22:02:52 -0300

    salvando tipo de variáveis

--- meu-primeiro-projeto-java/src/TiposVariaveis.java
+++ meu-primeiro-projeto-java/src/TiposVariaveis.java
@@ -1,3 +1,10 @@
 public class TiposVariaveis {
-    
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;//convenção americana
+        float pi = 3.14F;//tipo float sempre tem o f no final
+        long cpf = 297834512668L;//com long a mesma coisa
+
+
+        final double VALOR_DE_PAR_2 = 2;//constante sempre terá final, e deve ser escrito em caixa alta
+    }
 }
