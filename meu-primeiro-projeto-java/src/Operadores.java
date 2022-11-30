30b9e0c5f1e1eb95d12770d5a6fb00e93d3f61d5
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2022-11-29 22:50:24 -0300

    Aula de operadores

---
+++ meu-primeiro-projeto-java/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int idade = 22;
+        String nome = "Rafael";
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+    }
+}
